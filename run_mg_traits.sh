309878c5ffbdecf8fd1f59b48c2e3d729cffcc5c
pereiramemo <pereiramemo@gmail.com>
2023-06-01 23:25:35 +0000

    added run script

---
+++ run_mg_traits.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -o errexit
+
+function realpath() {
+  CURRENT_DIR=$(pwd)
+  DIR=$(dirname "${1}")
+  FILE=$(basename "${1}")
+  cd "${DIR}"
+  echo $(pwd)/"${FILE}"
+  cd "${CURRENT_DIR}"
+}
+
+# check input parameters
+if [[ "$#" -lt 3 ]]; then
+  echo -e "Missing parameters.\nSee run_mg_traits.sh . . --help"
+  exit
+fi
+
+# handle input fna file
+INPUT_FNA=$(basename "${1}")
+INPUT_DIR=$(dirname $(realpath "${1}"))
+shift
+
+OUTPUT_DIR=$(dirname $(realpath "${1}"))
+OUTPUT=$(basename "${1}")
+shift
+
+# links within the container
+CONTAINER_SRC_DIR=/input
+CONTAINER_DST_DIR=/output
+
+docker run \
+  --volume "${INPUT_DIR}":"${CONTAINER_SRC_DIR}":rw \
+  --volume "${OUTPUT_DIR}":"${CONTAINER_DST_DIR}":rw \
+  --detach=false \
+  --rm \
+  --user $(id -u):$(id -g) \
+   epereira/mg_traits:latest \
+  --input_fna "${CONTAINER_SRC_DIR}/${INPUT_FNA}" \
+  --outdir "${OUTPUT}" \
+  $@
